1414306237fb611963adb52b88aba8d19ba0cfc6
nilaysinghsolanki <nilaysolanki@outlook.com>
2021-06-18 21:45:38 +0530

    Commit new

---
+++ lib/Screens/addEventScreen.dart
@@ -0,0 +1,272 @@
+import 'package:nilay_dtuotg_2/models/screenArguments.dart';
+import 'package:nilay_dtuotg_2/providers/info_provider.dart';
+import 'package:nilay_dtuotg_2/providers/server_connection_functions.dart';
+import 'package:nilay_dtuotg_2/widgets/eventDurationPicker.dart';
+import 'package:nilay_dtuotg_2/widgets/eventOwnerPicker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:dropdown_formfield/dropdown_formfield.dart';
+
+class AddEventScreen extends StatefulWidget {
+  static const routeName = 'AddEventScreen';
+  AddEventScreen({Key key}) : super(key: key);
+
+  @override
+  _AddEventScreenState createState() => _AddEventScreenState();
+}
+
+class _AddEventScreenState extends State<AddEventScreen> {
+  final name = TextEditingController(
+
+  );
+  final description = TextEditingController();
+  @override
+  void initState() {
+    name.text = 'name';
+    description.text = 'description';
+    // TODO: implement initState
+    super.initState();
+  }
+
+  DateTime dateTime;
+  TimeOfDay timeOfDay;
+
+  final formGlobalKey = GlobalKey<FormState>();
+  bool waiting = false;
+  var resp;
+  int type;
+  DateTime now = DateTime.now();
+  Duration _duration = Duration(hours: 0, minutes: 0);
+  @override
+  Widget build(BuildContext context) {
+    // BuildContext bc =
+    //     Provider.of<TabsScreenContext>(context, listen: false).get();
+    var data = Provider.of<AddEventScreenData>(context, listen: true);
+    return Scaffold(
+      backgroundColor:  Colors.brown,
+      persistentFooterButtons: [
+        ElevatedButton.icon(
+
+          style: ElevatedButton.styleFrom(
+            primary: Colors.white ,
+            elevation: 0
+          ),
+
+            onPressed: () async {
+              print('1');
+              setState(() {
+                waiting = true;
+              });
+              if (dateTime != null &&
+                  timeOfDay != null &&
+                  name != null &&
+                  description != null &&
+                  type != null) {
+                print('2');
+                var scf = Provider.of<SCF>(context, listen: false).get();
+                Map<String, dynamic> resp = await scf.createEvent(context,
+                    name.text, description.text, type, dateTime, timeOfDay);
+                scf.fetchListOfEvents(context);
+
+                print('3');
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return Dialog(
+                        child: Padding(
+                          padding: EdgeInsets.all(50),
+                          child: Text(resp.toString()),
+                        ),
+                      );
+                    });
+
+                if (resp["status"] == "OK") {
+                  Navigator.of(context).pop();
+                }
+              }
+              setState(() {
+                waiting = false;
+              });
+            },
+            icon: Icon(Icons.save,color: Colors.brown,),
+            label: waiting ? CircularProgressIndicator() : Text('save',style: TextStyle(color: Colors.brown),))
+
+      ],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('add event'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage("Assets/newframe.png"), fit: BoxFit.cover),
+            borderRadius: BorderRadius.circular(20)),
+
+        child: SingleChildScrollView(
+          child: Form(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  primary: Colors.white,
+                  side: BorderSide(color: Colors.brown,width: 2)
+                ),
+                child: Text('Pick A Date',style: TextStyle(color: Colors.brown),),
+                autofocus: true,
+                clipBehavior: Clip.hardEdge,
+                onPressed: () async {
+                  dateTime = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime(2021),
+                      firstDate: DateTime(2021),
+                      lastDate: DateTime(2022));
+                  print('$dateTime');
+                },
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    primary: Colors.white,
+                    side: BorderSide(color: Colors.brown,width: 2)
+                ),
+                onPressed: () async {
+                  timeOfDay = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay(hour: 1, minute: 0),
+                  );
+                  print('$timeOfDay');
+                },
+                child: Text("startingTime?",style: TextStyle(color: Colors.brown),),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage("Assets/newframe.png"), fit: BoxFit.cover),),
+
+                child: ListTile(
+
+                  leading: Icon(Icons.timelapse),
+                  tileColor: Colors.white,
+                  title: Text('Duration?'),
+                  trailing: Text('${data.getHours()}h ${data.getMinutes()}min'),
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return Dialog(child: DurationPicker());
+                        });
+                  },
+                ),
+              ),
+              // ListTile(
+              //   leading: Icon(Icons.people),
+              //   tileColor: Colors.blue[200],
+              //   title: Text('owners'),
+              //   trailing: Text('${data.getOwners()}'),
+              //   onTap: () {
+              //     showDialog(
+              //         context: context,
+              //         builder: (context) {
+              //           return Dialog(child: OwnerPicker());
+              //         });
+              //   },
+              // ),
+              Padding(
+                child: CupertinoTextFormFieldRow(
+                  cursorColor: Colors.brown,
+
+                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20))
+                  ,color: Colors.white),
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+
+                  maxLength: 20,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                  controller: name,
+                  //  restorationId: 'username',
+                  placeholder: 'Name of the event',
+                  keyboardType: TextInputType.emailAddress,
+                  //   clearButtonMode: OverlayVisibilityMode.editing,
+                  obscureText: false,
+                  autocorrect: false,
+                  validator: (value) {
+                    if (value.length > 20) {
+                      return 'string too long';
+                    }
+                    if (value.isEmpty) {
+                      return 'enter name';
+                    }
+                  },
+                ),
+                padding:
+                    EdgeInsets.only(left: 22, top: 0, bottom: 20, right: 22),
+              ),
+              Padding(
+                child: CupertinoTextFormFieldRow(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(20))
+                      ,color: Colors.white),
+                  maxLength: 2000, maxLines: 8, minLines: 1,
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                  controller: description,
+                  //  restorationId: 'username',
+                  placeholder: "",
+                  keyboardType: TextInputType.multiline,
+                  //   clearButtonMode: OverlayVisibilityMode.editing,
+                  obscureText: false,
+                  autocorrect: false,
+                  validator: (value) {
+                    if (value.length > 2000) {
+                      return 'string too long';
+                    }
+                    if (value.isEmpty) {
+                      return 'enter description';
+                    }
+                  },
+                ),
+                padding:
+                    EdgeInsets.only(left: 22, top: 0, bottom: 20, right: 22),
+              ),
+
+              Container(
+                padding: EdgeInsets.all(20),
+                child: DropDownFormField(
+                  textField: 'display',
+                  valueField: 'value',
+                  dataSource: [
+                    {
+                      "display": "type1",
+                      "value": 1,
+                    },
+                    {
+                      "display": "type2",
+                      "value": 2,
+                    },
+                    {
+                      "display": "type3",
+                      "value": 3,
+                    },
+                  ],
+                  onSaved: (value) {
+                    setState(() {
+                      type = value;
+                    });
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      type = value;
+                    });
+                  },
+                  titleText: 'event type',
+                  value: type,
+                  hintText: 'choose event type',
+                ),
+              ),
+
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/invite_screen.dart
@@ -0,0 +1,88 @@
+import 'package:nilay_dtuotg_2/providers/info_provider.dart';
+import 'package:nilay_dtuotg_2/providers/server_connection_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+class InviteScreen extends StatefulWidget {
+  static const routeName = "inviteScreen";
+  InviteScreen({Key key}) : super(key: key);
+
+  @override
+  _InviteScreenState createState() => _InviteScreenState();
+}
+
+class _InviteScreenState extends State<InviteScreen> {
+  bool waiting = false;
+  bool respCame = false;
+  var resp;
+  final email = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('invite friends'),
+      ),
+      body: Container(decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage("Assets/newframe.png"), fit: BoxFit.cover),
+      ),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Form(
+                child: Padding(
+                  child: CupertinoTextFormFieldRow(
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
+                    controller: email,
+                    //   restorationId: 'email',
+                    placeholder: 'email',
+                    keyboardType: TextInputType.name,
+                    // clearButtonMode: OverlayVisibilityMode.editing,
+                    obscureText: false,
+                    autocorrect: false,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'enter email';
+                      }
+                    },
+                  ),
+                  padding:
+                      EdgeInsets.only(left: 22, top: 0, bottom: 20, right: 22),
+                ),
+              ),
+              if (respCame) Text(resp.toString()),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.white ,
+                      elevation: 0
+                  ),
+                  onPressed: () async {
+                    setState(() {
+                      respCame = false;
+                      waiting = true;
+                    });
+                    var scf = Provider.of<SCF>(context, listen: false).get();
+                    resp = await scf.invite(email.text, context);
+                    setState(() {
+                      waiting = false;
+                      respCame = true;
+                    });
+                  },
+                  child: waiting
+                      ? CircularProgressIndicator(
+                          backgroundColor: Colors.amber,
+                        )
+                      : Text('invite',style: TextStyle(color: Colors.brown),))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
